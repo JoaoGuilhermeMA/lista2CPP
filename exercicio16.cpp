7f944d491111a3c4b8dfd9a02343752a0f279f5f
JoaoGuilhermeMA <joomoreira36@gmail.com>
2023-04-01 01:52:42 -0300

    adicionando o exercicio 16

---
+++ exercicio16.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int idade;
+    cout << "Quantos ano voce tem? " << endl;
+    cin >> idade;
+
+    if(idade < 18){
+        cout << "falta " << 18 - idade << " ano para voce poder tirar a carteira de motorista" << endl;
+    }else{
+        cout << "Pode tirar a carteira";
+    }
+}
